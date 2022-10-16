6da33725311f223429aa0e80738db60d8ddccaa4
predmaxim <predmaxim@ya.ru>
2022-10-16 18:11:18 +0500

    refactor: hanoi tower change formula and clear

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -16,16 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function calculateHanoi(disksNumber, turnsSpeed) {
   const turns = (Math.pow(2, disksNumber) - 1)
-  const seconds = Math.floor((turns / turnsSpeed * 100) * 36)
+  const seconds = Math.floor((turns / turnsSpeed) * 3600)
   return { turns: turns, seconds: seconds }
 }
 
 module.exports = {
   calculateHanoi
 };
-
-// console.log(calculateHanoi(5, 4074))// { turns: 31, seconds: 27 }
-// console.log(calculateHanoi(38, 4594))// { turns: 274877906943, seconds: 215402800390 }
-// console.log(calculateHanoi(34, 4005))// { turns: 17179869183, seconds: 15442579040 }
-// console.log(calculateHanoi(19, 4770))// { turns: 524287, seconds: 395688 }
-// console.log(calculateHanoi(23, 4344))// { turns: 8388607, seconds: 6951884 }
